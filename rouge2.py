9fd3a2ddf91d183af3a3d13176c04a0b5b7215ab
Christian Mojallali <cmojallali@gmail.com>
2017-12-07 16:16:37 +0000

    new main script

---
+++ rouge2.py
@@ -0,0 +1,33 @@
+import curses
+import random
+import os
+
+os.system('clear')
+races = ['human',
+        'flying eyeball',
+        'robot',
+        'dino-person']
+
+curses.initscr()
+try:
+    win = curses.newwin(0,0)
+    win.addstr(0,2,'What is thy name? ')
+    pcname = win.getstr()
+    win.refresh()
+    
+    win.clear()
+    win.refresh()
+    y = 0
+    for race in races:
+        win.addstr(y,2,(str(y)+': '+str(race)))
+        y += 1
+    win.addstr(6,2, 'choose thine race: ')
+    racechoice = win.getstr()
+
+except:
+    curses.endwin()
+finally:
+    pass
+curses.endwin()
+print pcname
+print races[int(racechoice)]
