e9afd0931482ff6b3add86e8ab084c953383d930
Christian Mojallali <cmojallali@gmail.com>
2017-12-08 08:44:09 +0000

    @ is now moveable with hjklyubn

--- rouge2.py
+++ rouge2.py
@@ -19,16 +19,45 @@ try:
     win.addstr(6,2, 'choose thine race: ')
     racechoice = win.getstr()
     
+    pcy = random.randint(1,10)
+    pcx = random.randint(1,10)
+
     curses.curs_set(0)
-    win.clear()
-    win.refresh()
-    win.move(10,10)
-    win.addch(ord('@'))
-    win.getch()
+    while True:
+        # clears screen and draws character
+        win.clear()
+        win.refresh()
+        win.move(pcy,pcx)
+        win.addch(ord('@'))
+        
+        # deals with movement 
+        movement = win.getch()
+        if movement == 104:
+            pcx -= 1
+        elif movement == 106:
+            pcy += 1
+        elif movement == 107:
+            pcy -= 1
+        elif movement == 108:
+            pcx += 1
+        elif movement == 121:
+            pcx -= 1
+            pcy -= 1
+        elif movement == 117:
+            pcx += 1
+            pcy -= 1
+        elif movement == 98:
+            pcx -= 1
+            pcy += 1
+        elif movement == 110:
+            pcx += 1
+            pcy += 1
+
 except:
     pass
 finally:
     curses.endwin()
-#curses.endwin()
 #print pcname
 #print races[int(racechoice)]
+#y 121 | u 117 | b 98 | n 110
+# h 104 || j 106 || k 107 || l 108
